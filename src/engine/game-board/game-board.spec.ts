7d5952da2c8b4fbcd00bff991daf000af964e5b3
pawbor <p.borowski88@gmail.com>
2017-02-28 17:04:06 +0100

    game-board simplified (#5)

--- src/engine/game-board/game-board.spec.ts
+++ src/engine/game-board/game-board.spec.ts
@@ -4,46 +4,75 @@ import {
 
 import {
   createGameBoard,
-  GameBoard,
-  boardMatchesDefinition
+  validateGameBoardDef
 } from './game-board';
 
 describe('createGameBoard', () => {
-  it('should create a GameBoard object', () => {
-    const board = createGameBoard({spaces: []});
-    expect(board).toBeDefined();
+  it('should create a GameBoard object from GameBoardDef', () => {
+    const def = [[
+      BoardSpaceType.GROUND,
+      BoardSpaceType.TEMPLE
+    ], [
+      BoardSpaceType.WATER,
+      BoardSpaceType.GROUND
+    ]];
+    const board = createGameBoard(def);
+    const expected = def.map(
+      (row) => row.map(
+        (type) => jasmine.objectContaining({type}),
+      )
+    );
+    expect(board).toEqual(expected);
   });
 
-   describe('- created GameBoard object', () => {
-    const def = {
-      spaces: [[
-        BoardSpaceType.GROUND,
-        BoardSpaceType.WATER,
-        BoardSpaceType.TEMPLE
-      ]]
-    };
+  it('should throw an error if GameBoardDef is invalid', () => {
+    const def = [[
+      BoardSpaceType.TEMPLE
+    ], [
+      BoardSpaceType.WATER,
+      BoardSpaceType.GROUND
+    ]];
 
-    it('should match GameBoardDef object', () => {
-      const board = createGameBoard(def);
-      const match = boardMatchesDefinition(board, def);
-      expect(match).toBeTruthy(`${board} not matching ${def}`);
-    });
+    expect(() => createGameBoard(def)).toThrowError();
   });
 });
 
-
-describe('boardMatchesDefinition', () => {
-  const board = {spaces: [[{type: BoardSpaceType.GROUND}]]};
-  const def = {spaces: [[BoardSpaceType.GROUND]]};
-  const otherDef = {spaces: [[BoardSpaceType.WATER]]};
-
-  it('should return true when GameBoard match GameBoardDef', () => {
-    const result = boardMatchesDefinition(board, def);
-    expect(result).toBeTruthy();
+describe('validateGameBoardDef', () => {
+  it('should return undefined if GameBoardDef is valid', () => {
+    const def = [[
+      BoardSpaceType.GROUND
+    ], [
+      BoardSpaceType.WATER
+    ]];
+    const result = validateGameBoardDef(def);
+    expect(result).toBeUndefined();
   });
 
-  it('should return false when GameBoard doesn\'t match GameBoardDef', () => {
-    const result = boardMatchesDefinition(board, otherDef);
-    expect(result).toBeFalsy();
+  it('should return error string if GameBoardDef is empty', () => {
+    const def = [];
+    const result = validateGameBoardDef(def);
+    expect(result).toEqual('game-board.empty');
+  });
+
+  it('should return error string if first GameBoardDef row is empty', () => {
+    const def = [[], [
+      BoardSpaceType.WATER,
+      BoardSpaceType.GROUND
+    ]];
+
+    const result = validateGameBoardDef(def);
+    expect(result).toEqual('game-board.empty-row');
+  });
+
+  it('should return error string if GameBoardDef rows are not even', () => {
+    const def = [[
+      BoardSpaceType.TEMPLE
+    ], [
+      BoardSpaceType.WATER,
+      BoardSpaceType.GROUND
+    ]];
+
+    const result = validateGameBoardDef(def);
+    expect(result).toEqual('game-board.not-even');
   });
 });
