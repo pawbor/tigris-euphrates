bc6806c3c2b645212a96af60a0d4b1322503ecd3
Paweł Borowski <p.borowski88@gmail.com>
2017-02-28 22:15:40 +0100

    Stronger typing and index.ts

--- src/engine/leader/leader.ts
+++ src/engine/leader/leader.ts
@@ -1,32 +1,36 @@
+export type LeaderRole = 'Farmer' | 'King' | 'Priest' | 'Trader';
+
+export type Dynasty = 'Archer' | 'Bull' | 'Lion' | 'Pot';
+
 export interface Leader {
-  dynasty: string;
-  role: string;
+  dynasty: Dynasty;
+  role: LeaderRole;
 }
 
-export function createLeader(role: string, dynasty: string): Leader {
+export function createLeader(role: LeaderRole, dynasty: Dynasty): Leader {
   return {
     get dynasty() { return dynasty; },
     get role() { return role; }
   };
 }
 
-export function createFarmer(dynasty: string): Leader {
+export function createFarmer(dynasty: Dynasty): Leader {
   return createLeader('Farmer', dynasty);
 };
 
-export function createKing(dynasty: string): Leader {
+export function createKing(dynasty: Dynasty): Leader {
   return createLeader('King', dynasty);
 };
 
-export function createPriest(dynasty: string): Leader {
+export function createPriest(dynasty: Dynasty): Leader {
   return createLeader('Priest', dynasty);
 };
 
-export function createTrader(dynasty: string): Leader {
+export function createTrader(dynasty: Dynasty): Leader {
   return createLeader('Trader', dynasty);
 };
 
-export function createLeaders(dynasty: string): Leader[] {
+export function createLeaders(dynasty: Dynasty): Leader[] {
   return [
     createKing(dynasty),
     createPriest(dynasty),
