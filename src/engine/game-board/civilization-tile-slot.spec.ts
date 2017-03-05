0fcfe22f45968bf7a1cc92bacc5de80892eedb56
Paweł Borowski <p.borowski88@gmail.com>
2017-03-05 22:02:39 +0100

    unification tile slot

--- src/engine/game-board/civilization-tile-slot.spec.ts
+++ src/engine/game-board/civilization-tile-slot.spec.ts
@@ -1,6 +1,6 @@
 import { createEmptySlot } from './empty-slot';
 import { createAllMonuments } from 'engine/monument';
-import { createFarm } from 'engine/tile';
+import { createFarm, createUnificationTile } from 'engine/tile';
 import { createTreasureToken } from 'engine/token';
 
 import {
@@ -8,30 +8,31 @@ import {
   createCivilizationTileSlot
 } from './civilization-tile-slot';
 
-const tile = createFarm();
+const farm = createFarm();
 const monument = createAllMonuments()[0];
 const treasure = createTreasureToken();
+const unificationTile = createUnificationTile();
 
 describe('createCivilizationTileSlot', () => {
   it('should create CivilizationTileSlot', () => {
-    const slot = createCivilizationTileSlot(tile, monument, treasure);
+    const slot = createCivilizationTileSlot(farm, monument, treasure);
     const expected = jasmine.objectContaining({
-      type: 'CivilizationTile', tile, monument, treasure
+      type: 'CivilizationTile', tile: farm, monument, treasure
     });
     expect(slot).toEqual(expected);
   });
 });
 
 describe('CivilizationTileSlot', () => {
-  const withTile = createCivilizationTileSlot(tile, undefined, undefined);
-  const withMonument = createCivilizationTileSlot(tile, monument, undefined);
-  const withTreasure = createCivilizationTileSlot(tile, undefined, treasure);
+  const withTile = createCivilizationTileSlot(farm, undefined, undefined);
+  const withMonument = createCivilizationTileSlot(farm, monument, undefined);
+  const withTreasure = createCivilizationTileSlot(farm, undefined, treasure);
 
   describe('placeMonument', () => {
     it('should create CivilizationTileSlot with monument', () => {
       const slot = withTreasure.placeMonument(monument);
       const expected = jasmine.objectContaining({
-        type: 'CivilizationTile', tile, monument, treasure
+        type: 'CivilizationTile', tile: farm, monument, treasure
       });
       expect(slot).toEqual(expected);
       expect(withTreasure.monument).toBeUndefined();
@@ -48,7 +49,7 @@ describe('CivilizationTileSlot', () => {
     it('should create CivilizationTileSlot with treasure', () => {
       const slot = withMonument.placeTreasureToken(treasure);
       const expected = jasmine.objectContaining({
-        type: 'CivilizationTile', tile, monument, treasure
+        type: 'CivilizationTile', tile: farm, monument, treasure
       });
       expect(slot).toEqual(expected);
       expect(withMonument.treasure).toBeUndefined();
@@ -58,6 +59,16 @@ describe('CivilizationTileSlot', () => {
       const callFn = () => withTreasure.placeTreasureToken(treasure);
       const expected = 'civilization-tile-slot.treasure-placed';
       expect(callFn).toThrowError(expected);
+    });
+  });
+
+  describe('placeUnificationTile', () => {
+    it('should create UnificationTileSlot', () => {
+      const slot = withTile.placeUnificationTile(unificationTile);
+      const expected = jasmine.objectContaining({
+        type: 'UnificationTile', covered: withTile, tile: unificationTile
+      });
+      expect(slot).toEqual(expected);
     });
   });
 });
